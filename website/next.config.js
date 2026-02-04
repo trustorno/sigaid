21fb5408e624033cbb799431b2b944409695c045
trustorno <16194277+trustorno@users.noreply.github.com>
2026-02-04 16:10:30 +0100

    Configure static export for Amplify deployment

--- website/next.config.js
+++ website/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
-    domains: ['api.sigaid.com'],
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
